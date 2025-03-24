498c445612c032d214ecac9b852948c6f0f774f7
gfavo <gabriel.lfavaro@gmail.com>
2025-03-24 10:01:24 -0300

    Minor fixes

---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,9 @@
+const SideMenu = () => {
+  return (
+    <div className="w-100 h-100 border-right-3">
+      
+    </div>
+  );
+};
+
+export default SideMenu;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { Header } from "@/components/header";
+import { Header } from "@/components/Header";
 import { useAuth } from "../context/authContext";
 import { useLoginStore } from "@/store/store";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { User } from "@/models/user";
+import SideMenu from "@/components/SideMenu";
 
 export default function Dashboard() {
   const { user, checkSession } = useAuth();
@@ -14,11 +16,17 @@ export default function Dashboard() {
     checkSession();
   }, []);
 
-
   return (
-    <div>
-    <Header />
-    <div className="flexbox h-full w-full bg-gray-500/6"></div>
+    <div className="w-screen h-screen">
+      <Header />
+      <SideMenu />
+      <div className="flex justify-center pt-10 h-full w-full bg-gray-500/6">
+           
+        <h2 className="font-barlow text-4xl">
+          <span className="text-gray-600">Hello, </span>
+          <span className="">{(user as User)?.name}</span>!
+        </h2>
+      </div>
     </div>
   );
 }
